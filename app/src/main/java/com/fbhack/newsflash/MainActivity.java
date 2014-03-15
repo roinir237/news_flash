cfb3fbd7f70e26b8869f4072989c6b7e532f5542
Dorota <df1e12@soton.ac.uk>
2014-03-15 18:02:38 +0000

    spritz

--- app/src/main/java/com/fbhack/newsflash/MainActivity.java
+++ app/src/main/java/com/fbhack/newsflash/MainActivity.java
@@ -1,6 +1,7 @@
 package com.fbhack.newsflash;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -11,21 +12,27 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.TextView;
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.spritz_item);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        TextView spritz = (TextView) findViewById(R.id.spritz_view);
+        ArrayList<CharSequence> arrayList = new ArrayList<CharSequence>();
+        populateArrayList(arrayList);
+        countDown(spritz, arrayList);
 
-        startService(new Intent(this, FlashHead.class));
+       // startService(new Intent(this, FlashHead.class));
     }
 
 
@@ -60,9 +67,41 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.spritz_item, container, false);
             return rootView;
         }
+    }
+
+    private void populateArrayList(ArrayList<CharSequence> arrayList) {
+        arrayList.add("Make");
+        arrayList.add("sure");
+        arrayList.add("you've");
+        arrayList.add("installed");
+        arrayList.add("the");
+        arrayList.add("following");
+        arrayList.add("from");
+        arrayList.add("the");
+        arrayList.add("Android");
+        arrayList.add("SDK");
+        arrayList.add("Manager");
+        arrayList.add("before");
+        arrayList.add("building");
+    }
+
+    private void countDown(final TextView spritz, final ArrayList<CharSequence> arrayList) {
+        CountDownTimer timer = new CountDownTimer(10000, 10000) {
+            @Override
+            public void onTick(long l) {
+                spritz.setText(arrayList.get(0));
+            }
+
+            @Override
+            public void onFinish() {
+                arrayList.remove(0);
+                if (arrayList.size() != 0)
+                    countDown(spritz, arrayList);
+            }
+        }.start();
     }
 
 }
