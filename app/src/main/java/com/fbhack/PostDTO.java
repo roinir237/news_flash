0d594f6f8dc06507d68ba8354d856f264b0f4dcd
Shaun Dowling <shaun@duedil.com>
2014-03-15 21:27:55 +0000

    Added PostDTO interface

---
+++ app/src/main/java/com/fbhack/PostDTO.java
@@ -0,0 +1,21 @@
+package com.fbhack;
+
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shaundowling on 15/03/2014.
+ */
+public interface PostDTO {
+    public String getName();
+    public String getStatus();
+
+    public Bitmap getProfilePicture();
+    public Bitmap getPostedImage();
+
+    public List<String> getLikers();
+
+    public double getImportance();
+}
