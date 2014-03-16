436ad77357aaf56a75ee7fc0d25ec467efbce84a
Shaun Dowling <shaun@duedil.com>
2014-03-16 05:15:29 +0000

    Added central spritz view

--- app/src/main/java/com/fbhack/newsflash/StatusItem.java
+++ app/src/main/java/com/fbhack/newsflash/StatusItem.java
@@ -2,12 +2,14 @@ package com.fbhack.newsflash;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
+import android.util.DisplayMetrics;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
@@ -48,14 +50,42 @@ public class StatusItem extends CardItem {
     }
 
     public void swapView(WindowManager windowManager) {
-        WindowManager.LayoutParams params = (WindowManager.LayoutParams) view.getLayoutParams();
-
         if (!spritzView) {
             windowManager.removeView(this.view);
-            windowManager.addView(spritzerTextView, params);
+
+
+            DisplayMetrics displaymetrics = new DisplayMetrics();
+            windowManager.getDefaultDisplay().getMetrics(displaymetrics);
+            int sheight = displaymetrics.heightPixels;
+            int swidth = displaymetrics.widthPixels;
+
+            int height = 250;
+            int width = 900;
+
+            WindowManager.LayoutParams newParams = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.TYPE_PHONE,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                    PixelFormat.TRANSLUCENT);
+
+            newParams.height = height;
+            newParams.width = width;
+
+            newParams.x = swidth/2 - width / 2;
+            newParams.y = sheight/2 - height / 2;
+            newParams.gravity = Gravity.TOP | Gravity.LEFT;
+
+            windowManager.addView(spritzerTextView, newParams);
+
+            SpritzerTextView textView = (SpritzerTextView) spritzerTextView.findViewById(R.id.spritz_view_2);
+            textView.setTextSize(30);
+            new Spritzer(textView);
+
         } else {
+            WindowManager.LayoutParams params = (WindowManager.LayoutParams) view.getLayoutParams();
             windowManager.removeView(spritzerTextView);
-//        params.width = 200;
+
             windowManager.addView(this.view, params);
         }
 
